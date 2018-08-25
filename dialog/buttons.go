212102c413bcda1b336153f3919c2a3836803828
Amy <amyadzuki@gmail.com>
2018-08-25 18:51:41 +0000

    Update buttons.go

--- dialog/buttons.go
+++ dialog/buttons.go
@@ -1,6 +1,6 @@
 package dialog
 
-import "github.com/amy911/amy911/str"
+import "github.com/suite911/str911/str"
 
 // From <https://docs.microsoft.com/en-us/windows/desktop/uxguide/top-violations>
 //     "Present the commit buttons in the following order:"
